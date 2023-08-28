b355ca4f0715f929856e126aebb85f7b68b183eb
AkiraMatthew <akira.dev.math@gmail.com>
2023-08-28 13:30:08 -0300

    helper-hardhat-config and deploy fundme update

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -1,4 +1,4 @@
-import { network } from "hardhat";
+// import
 //main
 //calling for the main function
 
@@ -11,11 +11,30 @@ import { network } from "hardhat";
 // };
 // or
 
+import { network } from "hardhat";
+import { networkConfig } from "../helper-hardhat-config";
+
 export default async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const chainId = network.config.chainId;
+    const chainId: number = network.config.chainId;
+
+    // if chainId is X use address Y
+    // if chainId is Z use address A
+
+    const ethUsdPriceFeedAddress: string =
+        networkConfig[chainId]["ethUsdPricefeed"];
+
+    // if the contract doesn't exist, we deploy a minimal version of
+    // for our local testing
 
     // what happens when we want to change chains?
     // when going for localhost or hardhat network we want to use a mock
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [
+            /* address? */
+        ], // put price feed address
+        log: true,
+    });
 };
